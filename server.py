16542ea5b187679dfc4e35f67f76373694f7f846
Ömer Faruk <leninsfriend@gmail.com>
2018-11-06 21:46:20 +0300

    add bootstrap

--- server.py
+++ server.py
@@ -28,6 +28,29 @@ recipes = [
         'title': 'Beef Stroganoff',
         'image_path': '6.jpg'
     },
+        {
+        'title': 'Sushi',
+        'image_path': '1.jpg'
+    },
+    {
+        'title': 'Ratatouille',
+        'image_path': '2.jpg'
+    },
+    {
+        'title': 'Fish & Chips',
+        'image_path': '3.jpg'
+    },
+    {
+        'title': 'New York Steak',
+        'image_path': '4.jpg'
+    },
+    {
+        'title': 'Kung Pao Chicken',
+        'image_path': '5.jpg'
+    },{
+        'title': 'Beef Stroganoff',
+        'image_path': '6.jpg'
+    }
 ]
 
 @app.route("/")
