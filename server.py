9aaf9221284b2a5d6b1b4d12320a24104143c4e2
Berkan <32255826+pikrua@users.noreply.github.com>
2018-11-06 17:20:45 +0300

    Add recipes as variables temporarily to server.py

--- server.py
+++ server.py
@@ -1,13 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 
 app = Flask(__name__)
 
+recipes = [
+    {
+        'title': 'Sushi',
+        'image_path': '1.jpg'
+    },
+    {
+        'title': 'Ratatouille',
+        'image_path': '2.jpg'
+    },
+    {
+        'title': 'Fish & Chips',
+        'image_path': '3.jpg'
+    },
+    {
+        'title': 'New York Steak',
+        'image_path': '4.jpg'
+    },
+    {
+        'title': 'Kung Pao Chicken',
+        'image_path': '5.jpg'
+    },{
+        'title': 'Beef Stroganoff',
+        'image_path': '6.jpg'
+    },
+]
 
 @app.route("/")
-def home_page():
-    return "Testing222..."
+@app.route("/home")
+def home():
+    return render_template('home.html', posts = recipes)
 
+@app.route("/about")
+def about():
+    return render_template('about.html', title = 'About')
 
 if __name__ == "__main__":
     app.run()
