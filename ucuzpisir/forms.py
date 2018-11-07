6857b7c721158454fd43f6d0342fa7dbc3318aa0
Berkan <32255826+pikrua@users.noreply.github.com>
2018-11-07 23:04:19 +0300

    Add validations on register form

--- ucuzpisir/forms.py
+++ ucuzpisir/forms.py
@@ -14,7 +14,20 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
-
+    def validate_username(self, username):
+        """
+        #following check is on sqlalchemy, fix this
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+        """
+    def validate_email(self, email):
+        """
+        #following check is on sqlalchemy, fix this
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
+        """
 class LoginForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
