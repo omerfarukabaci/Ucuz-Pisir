ca318841e4df52054da959b503db635e46d9d09e
Berkan <32255826+pikrua@users.noreply.github.com>
2018-11-07 22:41:52 +0300

    Add repr dunder for recipe, fix default values for pics

--- ucuzpisir/tables.py
+++ ucuzpisir/tables.py
@@ -3,7 +3,7 @@
 
 class User:
     __init__(self, user_id, username, email, password, birthdate, 
-            pic=url_for('static', filename="imgs/"+recipe.image_path)):
+                pic=url_for('static', filename="imgs/defaultProfile.jpg")):
         user_id = user_id;
         username = username;
         email = email;
@@ -15,7 +15,13 @@ class User:
         return f"User('{self.username}', '{self.email}')"
 
 class Recipe:
-    __init__(self, user_id, recipe_text, ingridients):
+    __init__(self, user_id, title, recipe_text, ingridients, date_posted,
+                recipe_img=url_for('static', filename="imgs/defaultRecipe.jpg)):
         user_id = user_id;
+        title = title;
         recipe_text = recipe_text;
         ingridients = ingridients;
+        date_posted = date_posted;
+        recipe_img = recipe_img
+    __repr__():
+        return f"Post('{self.title}', '{self.date_posted}')"
