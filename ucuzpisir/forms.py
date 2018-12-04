f2f53501a3430a67cd23f48e7c024eafabf82dc4
Berkan <berkanunal@gmail.com>
2018-12-04 22:00:40 +0300

    Add name and birthdate to registration form

--- ucuzpisir/forms.py
+++ ucuzpisir/forms.py
@@ -1,11 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, DateTimeField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)])
+    name = StringField('Name',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    birthdate = DateTimeField('Your birthdate', format='%d/%m/%y')
     email = StringField('Email',
                         validators=[DataRequired(), Email(), Length(max=80)])
     password = PasswordField('Password',
