e1e1dcc8e90cee51689bd95972f0bc188e1a24f9
Berkan <berkanunal@gmail.com>
2018-12-04 22:04:21 +0300

    Add skeleton of base class,  fix syntax

--- ucuzpisir/tables.py
+++ ucuzpisir/tables.py
@@ -1,27 +1,73 @@
 #import ??
+class Base:
+    def __init__(self):
+        self.connection_url = os.getenv("DATABASE_URL")
 
+    def create(self):
+        pass
+    
+    def update(self):
+        pass
+    
+    def retrieve(self):
+        pass
+    
+    def delete(self):
+        pass
+    
+    def execute(self, statement):
+        response = None
+        with dbapi2.connect(self.connection_url) as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(statement)
+                response = cursor.fetchall()
+        return response
 
-class User:
+class User(Base):
     __init__(self, user_id, username, email, password, birthdate, 
                 pic=url_for('static', filename="imgs/defaultProfile.jpg")):
-        user_id = user_id;
-        username = username;
-        email = email;
-        password = password;
-        birthdate = birthdate;
-        pic = pic;
+        user_id = user_id
+        username = username
+        email = email
+        password = password
+        birthdate = birthdate
+        pic = pic
 
     __repr__(self):
         return f"User('{self.username}', '{self.email}')"
 
-class Recipe:
+    def create(self):
+        statement = """
+        temp
+        """
+        self.execute(statement)
+    
+    def update(self):
+        statement = """
+        temp
+        """
+        self.execute(statement)
+    
+    def retrieve(self):
+        statement = """
+        temp
+        """
+        return self.execute(statement)
+    
+    def delete(self):
+        statement = """
+        temp
+        """
+        self.execute(statement)
+
+class Recipe(Base):
     __init__(self, user_id, title, recipe_text, ingridients, date_posted,
-                recipe_img=url_for('static', filename="imgs/defaultRecipe.jpg)):
-        user_id = user_id;
-        title = title;
-        recipe_text = recipe_text;
-        ingridients = ingridients;
-        date_posted = date_posted;
+                recipe_img=url_for('static', filename="imgs/defaultRecipe.jpg")):
+        user_id = user_id
+        title = title
+        recipe_text = recipe_text
+        ingridients = ingridients
+        date_posted = date_posted
         recipe_img = recipe_img
     __repr__():
         return f"Post('{self.title}', '{self.date_posted}')"
