bff1557d70b8fa496129e4f75ea8bf0478fb7b19
Berkan <berkanunal@gmail.com>
2018-12-04 22:24:24 +0300

    Format document as pep8 style

--- ucuzpisir/tables.py
+++ ucuzpisir/tables.py
@@ -1,20 +1,20 @@
-#import ??
+# import ??
 class Base:
     def __init__(self):
         self.connection_url = os.getenv("DATABASE_URL")
 
     def create(self):
         pass
-    
+
     def update(self):
         pass
-    
+
     def retrieve(self):
         pass
-    
+
     def delete(self):
         pass
-    
+
     def execute(self, statement):
         response = None
         with dbapi2.connect(self.connection_url) as connection:
@@ -23,9 +23,9 @@ class Base:
                 response = cursor.fetchall()
         return response
 
+
 class User(Base):
-    __init__(self, user_id, username, email, password, birthdate, 
-                pic=url_for('static', filename="imgs/defaultProfile.jpg")):
+    __init__(self, user_id, username, email, password, birthdate, pic=url_for('static' filename="imgs/defaultProfile.jpg")):
         user_id = user_id
         username = username
         email = email
@@ -41,28 +41,29 @@ class User(Base):
         temp
         """
         self.execute(statement)
-    
+
     def update(self):
         statement = """
         temp
         """
         self.execute(statement)
-    
+
     def retrieve(self):
         statement = """
         temp
         """
         return self.execute(statement)
-    
+
     def delete(self):
         statement = """
         temp
         """
         self.execute(statement)
 
+
 class Recipe(Base):
     __init__(self, user_id, title, recipe_text, ingridients, date_posted,
-                recipe_img=url_for('static', filename="imgs/defaultRecipe.jpg")):
+             recipe_img=url_for('static', filename="imgs/defaultRecipe.jpg")):
         user_id = user_id
         title = title
         recipe_text = recipe_text
@@ -78,19 +79,19 @@ class Recipe(Base):
         temp
         """
         self.execute(statement)
-    
+
     def update(self):
         statement = """
         temp
         """
         self.execute(statement)
-    
+
     def retrieve(self):
         statement = """
         temp
         """
         return self.execute(statement)
-    
+
     def delete(self):
         statement = """
         temp
