467e8d428b25370602f63ad05ec92eaf3d3f5b71
Berkan <berkanunal@gmail.com>
2018-12-05 16:50:35 +0300

    add db_local config

--- development.py
+++ development.py
@@ -1,4 +1,6 @@
 from ucuzpisir import app
 
+app.config["DATABASE_URI"] = "host='localhost' dbname='ucuzdb' user='postgres' password='.abc020615'"
+
 if __name__ == "__main__":
     app.run(debug=True)
