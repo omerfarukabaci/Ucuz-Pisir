4285bbf1d07d4cd1d14afb91237c23ec32909b20
Ömer Faruk <leninsfriend@gmail.com>
2018-12-05 13:17:36 +0300

    correct syntax

--- ucuzpisir/routes.py
+++ ucuzpisir/routes.py
@@ -68,15 +68,20 @@ def register():
     if form.validate_on_submit():
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
+    else:
+        print('Error!')
     return render_template('register.html', title='Register', form=form)
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
+        print('Success!')
         if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
+            flash(f'You have been logged in!', 'alert alert-success')
             return redirect(url_for('home'))
         else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
+            flash(f'Login Unsuccessful. Please check username and password', 'danger')
+    else:
+        print('Error!')
     return render_template('login.html', title='Login', form=form)
