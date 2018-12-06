674364a1b2f73145460f0286f2416427bffffb5f
Berkan <berkanunal@gmail.com>
2018-12-06 13:36:57 +0300

    remove development.py from cache

--- development.py
+++
@@ -1,6 +0,0 @@
-from ucuzpisir import app
-
-app.config["DATABASE_URI"] = "host='localhost' dbname='ucuzdb' user='postgres' password='.abc020615'"
-
-if __name__ == "__main__":
-    app.run(debug=True)
