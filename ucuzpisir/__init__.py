6f4c72139185814e25095dc2eda5f387b2672820
Ömer Faruk <leninsfriend@gmail.com>
2018-12-10 02:41:48 +0300

    configure login_manager

--- ucuzpisir/__init__.py
+++ ucuzpisir/__init__.py
@@ -7,5 +7,7 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'alert alert-info'
 
 from ucuzpisir import routes #Reminder: There is a reason this line is here but not on top.
