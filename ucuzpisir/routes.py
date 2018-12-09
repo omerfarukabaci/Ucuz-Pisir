5ed448b8c813e2c1ec9b81b0fd85684008fe2a6f
Ömer Faruk <leninsfriend@gmail.com>
2018-12-10 02:41:17 +0300

    add login requirement to account route, go where user left before login

--- ucuzpisir/routes.py
+++ ucuzpisir/routes.py
@@ -1,5 +1,5 @@
-from flask import render_template, url_for, flash, redirect
-from flask_login import login_user, current_user, logout_user
+from flask import render_template, url_for, flash, redirect, request
+from flask_login import login_user, current_user, logout_user, login_required
 from ucuzpisir import app, bcrypt
 from ucuzpisir.forms import RegistrationForm, LoginForm
 from ucuzpisir.tables import Base, User
@@ -56,7 +56,11 @@ def login():
             
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next') 
+            if next_page:
+                return redirect(next_page)
+            else:
+                return redirect(url_for('home'))
         else:
             flash(f'Login Unsuccessful. Please check e-mail and password', 'alert alert-danger')
     return render_template('login.html', title='Login', form=form)
@@ -68,5 +72,6 @@ def logout():
 
 
 @app.route("/account")
+@login_required
 def account():
     return render_template('account.html', title='Account')
