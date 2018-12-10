979ae6d0b4cfc694dd81bfe3156fffbc8db994ad
Berkan <berkanunal@gmail.com>
2018-12-10 21:23:34 +0300

    Add AccountUpdateForm class, Import current_user

--- ucuzpisir/forms.py
+++ ucuzpisir/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, DateTimeField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from ucuzpisir.tables import User
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -38,3 +39,30 @@ class LoginForm(FlaskForm):
                              validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class AccountUpdateForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    name = StringField('Name',
+                       validators=[DataRequired(), Length(min=2, max=20)])
+    birthdate = DateTimeField('Your birthdate', format='%d/%m/%Y',
+                              render_kw={"placeholder": "DD/MM/YYYY"})
+    email = StringField('Email',
+                        validators=[DataRequired(), Email(), Length(max=80)])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username
+            user = User().retrieve('username', f"username = '{username.data}'")
+            if user:
+                raise ValidationError(
+                    'That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email
+
+        user = User().retrieve('email', f"email = '{email.data}'")
+        if user:
+            raise ValidationError(
+                'That email is taken. Please choose a different one.')
+
