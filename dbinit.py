1f08ba761078542db6d4b30d0acc65e8e626dcfb
Ömer Faruk <leninsfriend@gmail.com>
2018-12-12 19:00:30 +0300

    Add new init statements, Add default_recipe image

--- dbinit.py
+++ dbinit.py
@@ -12,6 +12,14 @@ INIT_STATEMENTS = [
         DATE_UPLOADED DATE DEFAULT current_date
     )
     """,
+        """CREATE TABLE IF NOT EXISTS RECIPE_IMAGES(
+        IMG_ID SERIAL PRIMARY KEY,
+        FILENAME VARCHAR(20) UNIQUE NOT NULL,
+        EXTENSION VARCHAR(7) NOT NULL,
+        IMG_DATA BYTEA NOT NULL,
+        DATE_UPLOADED DATE DEFAULT current_date
+    )
+    """,
     """CREATE TABLE IF NOT EXISTS USERS(
         USER_ID SERIAL PRIMARY KEY,
         NAME VARCHAR(50) NOT NULL,
@@ -24,11 +32,11 @@ INIT_STATEMENTS = [
     """,
     """CREATE TABLE IF NOT EXISTS RECIPES(
         RECIPE_ID SERIAL PRIMARY KEY,
-        TITLE VARCHAR(80) NOT NULL,
+        TITLE VARCHAR(50) NOT NULL,
         CONTENT TEXT NOT NULL,
         PIC VARCHAR(20) DEFAULT 'defaultRecipe.jpg',
         DATE_POSTED TIMESTAMP,
-        USER_ID INTEGER REFERENCES USERS(USER_ID)
+        IMG_ID INTEGER REFERENCES RECIPE_IMAGES(IMG_ID) ON DELETE SET DEFAULT DEFAULT 1
     )
     """
 ]
@@ -43,7 +51,7 @@ def initialize(url):
     insertDefaultImage(url)
 
 
-def insertDefaultImage(url):
+def insertDefaultImages(url):
     with open('ucuzpisir/static/imgs/default_user.jpg', 'rb') as f:
         imageData = f
         defaultImage = User_image(url=url, filename='default', extension='jpeg',
